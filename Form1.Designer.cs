144848a8da1f004e2f08cf9b1f854217d54260f2
Ariana <ariana.calderon.20isc@tecsanpedro.edu.mx>
2022-04-07 13:04:11 -0500

    Primer Actualización

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace TAP_U2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CrearControl = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CrearControl
+            // 
+            this.CrearControl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CrearControl.Location = new System.Drawing.Point(102, 32);
+            this.CrearControl.Name = "CrearControl";
+            this.CrearControl.Size = new System.Drawing.Size(103, 23);
+            this.CrearControl.TabIndex = 0;
+            this.CrearControl.Text = "Crear Control";
+            this.CrearControl.UseVisualStyleBackColor = true;
+            this.CrearControl.Click += new System.EventHandler(this.CrearControl_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(102, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(381, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 281);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CrearControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CrearControl;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+
+namespace TAP_U2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Button button = new Button();
+        private void CrearControl_Click(object sender, EventArgs e)
+        {
+            //Crear conytoles, en tiempo de ejecución
+            //Instanciar una clase de tipo Button
+            string speech = string.Format("El nombre del control es:");
+
+            button.Name = "btn1";
+            button.Text = "soy nuevo";
+            //coordenadas del boton
+            button.Top = this.Height / 2;
+            button.Left = this.Width/2;
+            //tamaño del boton nuevo
+            button.Height = 50;
+            button.Width = 90;
+            //diseñodel boton
+            button.FlatStyle = FlatStyle.Flat;
+            //Este control hay que ponerlo en el form 1
+            this.Controls.Add(button);
+            //Asociar el button a un evento
+            button.Click += new EventHandler(btn1_click);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //
+            this.Controls.Remove(button);
+        }
+
+        private void btn1_click (object sender, EventArgs e)
+        {
+            this.BackColor = Color.Azure;
+            MessageBox.Show("Evento creado para este botón");
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            //capturar pantalla de nuestro SO
+            //instanciar           
+            Bitmap screen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g= Graphics.FromImage(screen))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, screen.Size);
+
+                   
+            }
+            pictureBox1.Image = screen;
+        }
+    }
+}
